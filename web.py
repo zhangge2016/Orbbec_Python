243b53bcdae6aabd2e8c530b394810dae6a362c6
zhangge <zhangge@yingzi.com>
2021-08-18 15:17:32 +0800

    flask web service

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 import os
+import cv2
 import numpy as np
 from flask import Flask, render_template, request
 
@@ -23,39 +24,53 @@ def return_img_stream(img_local_path):
     :return: 图片流
     """
     import base64
-    img_stream = ''
     with open(img_local_path, 'rb') as img_f:
         img_stream = img_f.read()
-        print(np.shape(img_stream))
 
         img_stream = base64.b64encode(img_stream).decode()
     return img_stream
 
 def get_recent_data():
-    root = r'E:\Download'
+    root = r'/data'
     if os.listdir(root) != None:
-        recent_hour = sorted(os.listdir(root))[0]
+        recent_hour = sorted(os.listdir(root), reverse=True)[0]
         rootdir = os.path.join(root, recent_hour)
         if os.listdir(rootdir) != None:
-            recent_ms = sorted(os.listdir(rootdir))[0]
+            recent_ms = sorted(os.listdir(rootdir), reverse=True)[0]
             img_path = os.path.join(rootdir, recent_ms)
             data = np.load(img_path, allow_pickle=True)
             depthPix, colorPix = data['depthPix'], data['colorPix']
             depthPix = 1 - 250 / (depthPix)
             depthPix[depthPix > 1] = 1
             depthPix[depthPix < 0] = 0
+            depthPix = depthPix*255
 
-            cv2.imwrite('depth.png', depthPix)
-            cv2.imwrite('color.png', colorPix)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            text = recent_ms.replace('.npz', '')
+            cv2.putText(colorPix, text, (10, 30), font, 0.75, (0, 0, 255), 2)
+            cv2.putText(depthPix, text, (10, 30), font, 0.75, (0, 0, 255), 2)
+            cv2.imwrite('images/depth.png', depthPix)
+            cv2.imwrite('images/color.png', colorPix)
+            return 1
+        else:
+            return 0
+    else:
+        return 0
 
 @app.route('/api/vis')
 def hello_world():
-    get_recent_data()
-    depth_stream = return_img_stream('depth.png')
-    color_stream = return_img_stream('color.png')
+    code = get_recent_data()
+    if code == 1:
+        depth_stream = return_img_stream('images/depth.png')
+        color_stream = return_img_stream('images/color.png')
+        return render_template('vis_index.html',
+                               depth_stream=depth_stream, color_stream=color_stream)
+    else:
+        init_stream = return_img_stream('images/init.png')
+        return render_template('vis_index.html',
+                               depth_stream=init_stream, color_stream=init_stream)
 
-    return render_template('vis_index.html',
-                           depth_stream=depth_stream, color_stream=color_stream)
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=False, use_reloader=False)
